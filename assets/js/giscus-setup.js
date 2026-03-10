3d7c66d608441919ab37724b7596d5babd185f02
sypark-dot <sypark-dot@users.noreply.github.com>
2026-03-10 04:06:35 +0000

    Deploying to gh-pages from @ sypark-dot/sypark-dot.github.io@be56e26172e2b3de4f5cf98b9663a1be15989d04 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "sypark-dot/sypark.git.io",
+    "data-repo": "sypark-dot/sypark-dot.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
